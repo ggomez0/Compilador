f6a5d339180c4d383cb81f5f92c5a1e648ddb69d
ggomez0 <gaspargomez2000@outlook.com>
2024-11-23 21:59:37 -0300

    change

--- AS.py
+++ AS.py
@@ -3,144 +3,96 @@ from AL import *
 import AL
 from Traductor import *
 
-primer_pin=False
-primer_mov=False
-
+primer_pin = False
+primer_mov = False
 
 def p_INICIO(p):
-  '''INICIO : EMPEZAR LIBRERIAS CUERPO TERMINAR'''
-  pass
+    '''INICIO : EMPEZAR LIBRERIAS CUERPO TERMINAR'''
+    pass
 
 def p_LIBRERIAS(p):
-  '''LIBRERIAS : LIBRERIASP
-                | EMPTY'''
-  pass
+    '''LIBRERIAS : LIBRERIASP
+                | empty'''
+    if len(p) > 1:
+        p[0] = p[1]
+    else:
+        p[0] = None
 
 def p_LIBRERIASP(p):
-  '''LIBRERIASP : extend LPAREN NOM_LIB RPAREN PTOCOMA LIBRERIAS
-                | extend LPAREN NOM_LIB RPAREN finLinea'''
-  traductor(p,trad_librerias)
-  pass
+    '''LIBRERIASP : extend LPAREN NOM_LIB RPAREN finLinea'''
+    p[0] = {'type': 'libraries', 'libs': p[3]}
+    traductor(p, trad_librerias)
 
 def p_CUERPO(p):
-  '''CUERPO : DEF_rule CUERPO
-        | ASSIGN_rule CUERPO
-        | FUNC CUERPO
-        | COMENTARIO CUERPO
-        | IF_rule CUERPO
-        | ELSE_rule CUERPO
-        | WHILE_rule CUERPO
-        | PIN_rule CUERPO
-        | MOV CUERPO
-        | EMPTY'''
-  pass
+    '''CUERPO : INSTRUCCION CUERPO
+              | empty'''
+    pass
+
+def p_INSTRUCCION(p):
+    '''INSTRUCCION : DEF_rule
+                  | PIN_rule
+                  | MOV
+                  | COMENTARIO'''
+    pass
 
 def p_DEF_rule(p):
-  '''DEF_rule : DEF TIPODATO nomVar ASSIGN VALORVAR finLinea'''
-  traductor(p,trad_def)
-  pass
+    '''DEF_rule : DEF TIPODATO nomVar finLinea
+               | DEF TIPODATO nomVar ASSIGN VALORVAR finLinea'''
+    traductor(p, trad_def)
+    pass
 
 def p_TIPODATO(p):
-  '''TIPODATO : palResInt
-        | palResString
-        | palResFloat
-        | palResBool'''
-  pass
-
-def p_ASSIGN_rule(p):
-  '''ASSIGN_rule : nomVar ASSIGN string finLinea
-        | nomVar ASSIGN int finLinea
-        | nomVar ASSIGN float finLinea
-        | nomVar ASSIGN true finLinea
-        | nomVar ASSIGN false finLinea
-        | nomVar ASSIGN nomVar finLinea'''
-  traductor(p,trad_asign)
-  pass
-
-def p_FUNC(p):
-  '''FUNC : nomVar LPAREN PARAM_LIST RPAREN RETORNO finLinea
-          | nomVar LPAREN PARAM_LIST RPAREN finLinea'''
-  traductor(p, trad_func)
-  pass
-
-def p_PARAM_LIST(p):
-  '''PARAM_LIST : TIPODATO nomVar
-                | TIPODATO nomVar PTOCOMA PARAM_LIST
-                | EMPTY'''
-  pass
-
-def p_COMENTARIO(p):
-  '''COMENTARIO : comBloque
-        | comLinea'''
-  pass
-
-def p_IF_rule(p):
-  '''IF_rule : IF LPAREN COMPARACION RPAREN LBRACE CUERPO RBRACE finLinea'''
-  traductor(p,trad_if)
-  pass
-
-def p_ELSE_rule(p):
-  '''ELSE_rule : ELSE LPAREN COMPARACION RPAREN LBRACE CUERPO RBRACE finLinea'''
-  pass
-
-def p_WHILE_rule(p):
-  '''WHILE_rule : WHILE LPAREN COMPARACION RPAREN LBRACE CUERPO RBRACE finLinea'''
-  pass
-
-def p_COMPARACION(p):
-  '''COMPARACION : VALORVAR comp VALORVAR
-        | nomVar comp VALORVAR
-        | nomVar comp nomVar
-        | false
-        | true
-        | nomVar'''
-  pass
-
-def p_VALORVAR(p):
-  '''VALORVAR : string
-        | true
-        | false
-        | int
-        | float'''
-  pass
-
+    '''TIPODATO : palResInt
+               | palResString
+               | palResFloat
+               | palResBool'''
+    p[0] = p[1]
 
 def p_PIN_rule(p):
-  '''PIN_rule : DEF PIN LPAREN PININ DOSPTOS INT RPAREN finLinea
-        | DEF PIN LPAREN PINOU DOSPTOS INT RPAREN finLinea'''
-  pass
-        
-  primer_pin = True if p_PIN_rule.counter <= 0 else False
-  p_PIN_rule.counter += 1
-  traductor(p, trad_pin, primer_pin=primer_pin, pin=True)
-  pass
-
-p_PIN_rule.counter = 0
-
+    '''PIN_rule : DEF PIN LPAREN PINOU DOSPTOS nomVar RPAREN finLinea
+               | DEF PIN LPAREN PININ DOSPTOS nomVar RPAREN finLinea'''
+    global primer_pin
+    primer_pin = True if not hasattr(p_PIN_rule, 'called') else False
+    p_PIN_rule.called = True
+    traductor(p, trad_pin, primer_pin=primer_pin, pin=True)
 
 def p_MOV(p):
-  '''MOV : ADEL LPAREN RPAREN finLinea
-        | ATR LPAREN RPAREN finLinea
-        | IZQ LPAREN RPAREN finLinea
-        | DER LPAREN RPAREN finLinea
-        | FREN LPAREN RPAREN finLinea
-        | ESP LPAREN int RPAREN finLinea
-        | ESP LPAREN float RPAREN finLinea'''
-  primer_mov = True if p_MOV.counter <= 0 else False
-  p_MOV.counter += 1
-  traductor(p,trad_mov,primer_mov=primer_mov, mov=True)
-  pass
+    '''MOV : ADEL LPAREN RPAREN finLinea
+           | ATR LPAREN RPAREN finLinea
+           | IZQ LPAREN RPAREN finLinea
+           | DER LPAREN RPAREN finLinea
+           | FREN LPAREN RPAREN finLinea
+           | ESP LPAREN int RPAREN finLinea
+           | ESP LPAREN float RPAREN finLinea'''
+    global primer_mov
+    primer_mov = True if not hasattr(p_MOV, 'called') else False
+    p_MOV.called = True
+    traductor(p, trad_mov, primer_mov=primer_mov, mov=True)
 
-p_MOV.counter=0
+def p_COMENTARIO(p):
+    '''COMENTARIO : comBloque
+                 | comLinea'''
+    pass
 
-def p_EMPTY(p):
-  '''EMPTY : '''
-  pass
+def p_VALORVAR(p):
+    '''VALORVAR : string
+               | true
+               | false
+               | int
+               | float
+               | nomVar'''
+    p[0] = p[1]
+
+def p_empty(p):
+    '''empty :'''
+    pass
 
 def p_error(p):
-  print("Error sintáctico en la línea: " + str(p.lineno)
-              + ". No se esperaba el token: " + str(p.value))        
-  raise Exception('syntax', 'error') 
-    
+    if p:
+        print(f"Error sintáctico en la línea {p.lineno}. Token inesperado: '{p.value}'")
+    else:
+        print("Error sintáctico: fin inesperado de entrada")
+
+# Construir el parser
 parser = yacc.yacc()
 AL.lexer.lineno=0
